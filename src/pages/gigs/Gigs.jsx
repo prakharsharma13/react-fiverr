d84f87ded466087ddbb1cca00fab52b6c82a8b8a
prakharsharma13 <115884239+prakharsharma13@users.noreply.github.com>
2023-08-06 23:32:47 +0530

    Completed

--- src/pages/gigs/Gigs.jsx
+++ src/pages/gigs/Gigs.jsx
@@ -1,10 +1,59 @@
-import React from 'react';
-import "./Gigs.scss"
+import React, { useState } from "react";
+import "./Gigs.scss";
+import { gigs } from "../../data";
+import GigCard from "../../components/gigCard/GigCard";
 
 const Gigs = () => {
-  return (
-    <div>Gigs</div>
-  )
-}
+  const [open, setOpen] = useState(false);
+  const [sort, setSort] = useState("sales");
 
-export default Gigs
+  const reSort = (type) => {
+    setSort(type);
+    setOpen(false);
+  };
+  return (
+    <div className="gigs">
+      <div className="container">
+        <span className="breadcrumbs">
+          Fiverr {">"} Graphics & Design {">"}
+        </span>
+        <h1>AI Artists</h1>
+        <p>
+          Explore the boundaries of art and technology with fiverr's AI artists
+        </p>
+        <div className="menu">
+          <div className="left">
+            <span>Budged</span>
+            <input type="text" placeholder="min" />
+            <input type="text" placeholder="max" />
+            <button>Apply</button>
+          </div>
+          <div className="right">
+            <span className="sortBy">SortBy</span>
+            <span className="sortType">
+              {sort === "sales" ? "Best Selling" : "Newest"}
+            </span>
+            <img src="./img/down.png" alt="" onClick={() => setOpen(!open)} />
+            {open && (
+              <div className="rightMenu">
+                {sort === "sales" ? (
+                  <span onClick={() => reSort("createdAt")}>Newest</span>
+                ) : (
+                  <span onClick={() => reSort("sales")}>Best Selling</span>
+                )}
+              </div>
+            )}
+          </div>
+        </div>
+
+        <div className="cards">
+          {gigs.map((gig) => (
+            <GigCard key={gig.id} item={gig} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Gigs;
